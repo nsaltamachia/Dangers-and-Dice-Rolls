342df2e054e4ff42c9c0c24a26eea7f90185ceed
NickSaltamachia <nsaltamachia@gmail.com>
2023-10-09 15:20:54 -0500

    finish 1st draft of html and css

---
+++ script.js
@@ -0,0 +1,39 @@
+// intialize game
+
+// game message " You've been attacked" Roll for initiative"
+
+// listen for player roll button click
+
+// generate random 1-20 for computer
+
+// compare the two intiative rolls
+
+// higher number sets turn
+
+// (if player wins initiative) game message " it's your turn, roll for attack!"
+
+// listen for player roll button click
+
+// generate random 1-20
+
+// subtract result from enemy healthbar
+
+// change player
+
+// game message "The enemy's attacking!! Watch out!"
+
+// generate random 1-20
+
+// subtract result from player healthbar
+
+// change player (repeat until winner found)
+
+// winner declared when first healthbar reaches <=0
+
+// game message: "________ won the battle! Press RESTART to play again!"
+
+// listen for RESTART button click
+
+// initialize game 
+
+
