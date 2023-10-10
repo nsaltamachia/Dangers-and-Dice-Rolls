e35a65ec9a19b47c0b0ae5ee4fca397e3face93e
NickSaltamachia <nsaltamachia@gmail.com>
2023-10-10 11:04:45 -0500

    update button click function

--- script.js
+++ script.js
@@ -1,8 +1,124 @@
+/*----- constants -----*/
+const players = {
+  '1': {
+    name: 'Player',
+    health: 50
+  },
+  '-1': {
+    name: 'Enemy',
+    health: 50
+  }
+};
+
+
+/*----- state variables -----*/
+let playerHealth = 50;
+let computerHealth = 50;
+let currentPlayer = "1";
+let currentPlayerRoll = 0;
+let currentComputerRoll = 0;
+
+
+/*----- cached elements  -----*/
+const rollEl = document.getElementById("rollButton");
+
+const gameMessageEl = document.querySelector(".message");
+console.log(gameMessageEl);
+const resetEl = document.getElementById("resetButton");
+
+const rollReadoutEl = document.getElementById("rollReadout");
+
+/*----- event listeners -----*/
+
+
+rollEl.addEventListener("click", () => {
+    const currentPlayerRoll = playerRoll();
+    console.log(currentPlayerRoll, "this is the player roll");
+    rollReadoutEl.innerHTML = `${currentPlayerRoll} / 20`;
+    const currentComputerRoll = playerRoll()
+    console.log(currentComputerRoll, "this is the computer roll");
+    displayGameMessage();
+})
+
+function playerRoll() {
+    return Math.floor(Math.random() * 20) + 1;
+}
+
+
+/*----- functions -----*/
 // intialize game
+// function initializeGame() {
+//     playerHealth = players['1'].health;
+//     computerHealth = players['-1'].health;
+//     currentPlayer = "1";
+//     displayGameMessage();
+    // listen for player roll button click
+    // generate random 1-20 for computer
+   
+// compare the two intiative rolls
 
-// game message " You've been attacked" Roll for initiative"
+// higher number sets turn
+// }
+// initializeGame();
 
-// listen for player roll button click
+
+
+// function initiativeRoll() {
+//     playerRoll();
+
+    
+//     
+
+// function rollRandom() {
+//     currentRoll = Math.floor(Math.random() * 20) + 1;
+// }
+
+function displayGameMessage() {
+    gameMessageEl.innerText = `The computer rolled a ${currentComputerRoll}`;
+    console.log(currentComputerRoll);
+    //if (currentPlayerRoll > )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // generate random 1-20 for computer
 
@@ -36,4 +152,16 @@
 
 // initialize game 
 
+
+
+//resetEl.addEventListener("click", resetGame);
+
+//function resetGame() {
+//     playerHealth = players["1"].health;
+//     computerHealth = players["-1"].health;
+//     currentPlayer = "1";
+//     gameMessageEl.innerText = "It looks like the ogre has more fight in him, lets go again!"
+    //////////////
+//     initializeGame();
+// }
 
