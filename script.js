d53081dd6225ca2ebf8686ff6809575785bd0c66
NickSaltamachia <nsaltamachia@gmail.com>
2023-10-12 10:23:41 -0500

    finish game function

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ gameMessageEl.innerText = `You've been ambushed
 /*----- event listeners -----*/
 
 rollEl.addEventListener("click", () => {
-    currentPlayerRoll = diceRoll();
+    currentPlayerRoll = rollDice();
     console.log(currentPlayerRoll, "this is the player roll");
     rollReadoutEl.innerHTML = `${currentPlayerRoll} / 20`;
-    currentComputerRoll = diceRoll()
+    currentComputerRoll = rollDice()
     computerRollTotal += currentComputerRoll;
     playerRollTotal += currentPlayerRoll;
     console.log(currentComputerRoll, "this is the computer roll");
@@ -92,7 +92,7 @@ function addRollToLists() {
     cPastRollsEl.innerHTML = cPastRolls;
 }
 
-function diceRoll() {
+function rollDice() {
     return Math.floor(Math.random() * 20) + 1;
 }
 
@@ -133,138 +133,30 @@ function checkforDraw() {
         Press RESET to play again!`;
     }
 
-
 function resetGame() {
-    let playerHealth = 100;
-    let computerHealth = 100;
-    let currentPlayer = "1";
-    let currentPlayerRoll = 0;
-    let currentComputerRoll = 0;
-    let pHealthBar = 100;
-    let pHealthNumber = 100;
-    let cHealthBar = 100;
-    let cHealthNumber = 100;
-    let playerRollTotal = 0;
-    let computerRollTotal = 0;
-    let pPastRolls = [];
-    let cPastRolls = [];
+    playerHealth = 100;
+    computerHealth = 100;
+    currentPlayer = "1";
+    currentPlayerRoll = 0;
+    currentComputerRoll = 0;
+    pHealthBar = 100;
+    pHealthNumber = 100;
+    cHealthBar = 100;
+    cHealthBarEl.style.opacity = 1;
+    pHealthBarEl.style.opacity = 1;
+    cHealthNumber = 100;
+    playerRollTotal = 0;
+    computerRollTotal = 0;
+    pPastRolls = [];
+    cPastRolls = [];
+    cHealthBarEl.style.width = "100%";
+    cHealthNumberEl.innerHTML = 100;
+    pHealthBarEl.style.width = "100%";
+    pHealthNumberEl.innerHTML = 100;
+    rollReadoutEl.innerHTML = "";
+    pPastRollsEl.innerHTML = "";
+    cPastRollsEl.innerHTML = "";
     gameMessageEl.innerText = `You've been ambushed 
     on your way to the castle!
     Click ROLL to attack!`;
-    cHealthBarEl.style.width = 100;
-    cHealthNumberEl.innerHTML = 100;
-    pHealthBarEl.style.width = 100;
-    pHealthNumberEl.innerHTML = 100;
-    rollReadoutEl.innerHTML = ``;
-    pPastRollsEl.innerHTML = "";
-    cPastRollsEl.innerHTML = "";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// generate random 1-20 for computer
-
-// compare the two intiative rolls
-
-// higher number sets turn
-
-// (if player wins initiative) game message " it's your turn, roll for attack!"
-
-// listen for player roll button click
-
-// generate random 1-20
-
-// subtract result from enemy healthbar
-
-// change player
-
-// game message "The enemy's attacking!! Watch out!"
-
-// generate random 1-20
-
-// subtract result from player healthbar
-
-// change player (repeat until winner found)
-
-// winner declared when first healthbar reaches <=0
-
-// game message: "________ won the battle! Press RESTART to play again!"
-
-// listen for RESTART button click
-
-// initialize game 
-
-
-
-
-
