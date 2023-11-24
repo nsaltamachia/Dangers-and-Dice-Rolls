b92dd3c37015d033230f8bdb4c42d89ff720b7c0
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-24 15:31:57 -0600

    adds README

---
+++ README.md
@@ -0,0 +1,48 @@
+**Nick Saltamachia SEI - Project 1 - Random Attack**
+
+This game is called Random Attack. It's based on the simple story of the player being ambushed by an ogre, 
+and uses random number generation to determine player attack and damage values with each turn. The goal of the game is to deplete the enemy ogre's health
+before they can do so to the player. The user purpose of the game is a little silly: I play Dungeons & Dragons with my friends and we're a bit of a superstitious bunch regarding luck and dice rolls: the main idea of the game is to get the bad rolls, like "natural 1's" out of the way prior to a campaign session...
+
+**Game Pictures and Game Play**
+![Game Load](https://github.com/nsaltamachia/Project1Game/assets/145282981/3ef0bbb3-9434-4fc0-99d1-a8a775cb89c6)
+The game loads with full player and computer stats on screen, a control panel for player action, and a message block that initially displays
+a brief story element and a CTA to begin the game by clicking ROLL. With each roll, the player's attack value is displayed in the control panel
+and the computer's attack value (player's damage value) is displayed in the game message panel. 
+
+
+
+![Mid Game](https://github.com/nsaltamachia/Project1Game/assets/145282981/3d974c5e-5a95-494a-becf-02060f0b126b)
+Throughout gameplay, as the player and computer take damage, their health bars and numerical health displays decrease according to each turn's attack/damage values.
+The player's and computer's roll history are also provided beneath their own health bar. With each turn, the game checks to see if the turn's damage values have decremented either the player's or the computer's health to equal-to-or-less-than zero.
+
+
+![Game End](https://github.com/nsaltamachia/Project1Game/assets/145282981/049cb4f7-cade-47d0-b785-18fb6b1e096a)
+When either the player's or computer's health reaches the game-end point a game message appears, indicating whether the player won or lost the battle and prompting the player to press RESET to play again. The game takes around 10 clicks/rolls to reach completion.
+
+**Technologies Used**
+I built the game with an HTML5 framework, using flex display to arrange the message planel, game stats & its contained elements, and control panel & its contained elements. Player controls are very simple, a ROLL button and a RESET button.
+
+I created the game background with Adobe Illustrator and the ogre was digitally illustrated by niece, Emma Campbell. I styles the game elements with CSS, mirroring tradtional "fighting game" elements like the health bar, and style the overall appearance to match the medieval/fantasty theme of the dice-rolling game, Dungeons & Dragons. I leaned for an all-ages, cartoonish, appearance with the look of the game, in an attempt to broaden the somewhat "niche" base of users who might already be familiar with the Dungeons & Dragons theme.
+
+I wrote the game's function/operation in "vanilla" Javascript, with the all game state changes for each "turn" resulting from one click of the ROLL button. Some of these changes include: random number generation, conditional element styling, conditional game messages, and presenting game state history.
+
+**Challenging Coding**
+The CSS was a little tricky, I believe I have a good grasp of using display: flex but I still have some trouble getting container elements (like the game message) to remain a certain size before its content appears. 
+
+Given the number of similarly-named elements, it was a little hard to keep them all straight. I could be more thoughtful of naming conventions, with consideration for how often a particular phrase would be repeated among elements.
+
+At the onset of building the game I was coding for a second player and with Ohz's help that, since it would be a computer opponent, I could streamline the number of functions needed for gameplay by building much of it into a one click event. That was certainly a relief and I fell like I've learned another thing to consider in the planning stages that will lead to better DRY coding.
+
+
+**Link to Game**
+
+https://nsaltamachia.github.io/Project1Game/
+
+**Next Steps**
+To improve this game over time, I would like to add some music and sound effects for certain roll results along with the game end event. I would also like to build different opponents with different attributes that would affect how much health they begin with, how much damage the deal and damage they inflict based on their dice rolls(buffs, perhaps). Perhaps those different opponents might inhabit entirely new "locations" or stages of the player story travel. I could also include certain limited-use buffs that the player would be able to utilize.
+
+To add some nuance to the game with regard to the simplicity of the player interaction and the majority of game logic coming from that one action, I will add some setTimeouts to the logic so that the user has something to "watch" after each click, instead of having all of each turn's effects occur at once.
+
+I will definitely go back to get the CSS the way I REALLY want it to look and implement responsive design to enable a mobile device experience.
+
---
+++ script.js
@@ -0,0 +1,187 @@
+/*----- constants -----*/
+const players = {
+  '1': {
+    name: 'Player',
+    health: 50
+  },
+  '-1': { //purely for two-human-player future development
+    name: 'Enemy',
+    health: 50
+  }
+};
+
+
+
+
+
+/*----- state variables -----*/
+let playerHealth = 100;
+let computerHealth = 100;
+let currentPlayer = "1";
+let currentPlayerRoll = 0;
+let currentComputerRoll = 0;
+let pHealthBar = 100;
+let pHealthNumber = 100;
+let cHealthBar = 100;
+let cHealthNumber = 100;
+let playerRollTotal = 0;
+let computerRollTotal = 0;
+let pPastRolls = [];
+let cPastRolls = [];
+
+
+
+
+/*----- cached elements  -----*/
+const rollEl = document.getElementById("rollButton");
+const gameMessageEl = document.querySelector(".message");
+//console.log(gameMessageEl);
+const resetEl = document.getElementById("resetButton");
+const rollReadoutEl = document.getElementById("rollReadout");
+let pHealthBarEl = document.querySelector(".pHealthBar");
+let pHealthNumberEl = document.getElementById("pHealthNumber");
+let cHealthBarEl = document.querySelector(".cHealthBar");
+let cHealthNumberEl = document.getElementById("cHealthNumber");
+let pPastRollsEl = document.getElementById("playerRolls");
+let cPastRollsEl = document.getElementById("computerRolls")
+
+gameMessageEl.innerText = `You've been ambushed 
+    on your way to the castle!
+    Click ROLL to attack!`;
+
+
+/*----- event listeners -----*/
+
+rollEl.addEventListener("click", () => {
+    currentPlayerRoll = rollDice();
+    console.log(currentPlayerRoll, "this is the player roll");
+    rollReadoutEl.innerHTML = `${currentPlayerRoll} / 20`;
+    currentComputerRoll = rollDice();
+    computerRollTotal += currentComputerRoll;
+    playerRollTotal += currentPlayerRoll;
+    console.log(currentComputerRoll, "this is the computer roll");
+    displayGameMessage();
+    updatePlayerStats();
+    updateComputerStats();
+    checkForWin()
+    addRollToLists();
+})
+
+resetEl.addEventListener("click", resetGame);
+
+
+
+
+
+/*----- functions -----*/
+function initializeGame() {
+    cHealthBarEl.style.width = 100;
+    cHealthNumberEl.innerHTML = 100;
+    pHealthBarEl.style.width = 100;
+    pHealthNumberEl.innerHTML = 100;
+    rollReadoutEl.innerHTML = ``;
+    gameMessageEl.innerText = `You've been ambushed 
+    on your way to the castle!
+    Click ROLL to fight back!`;
+}
+
+function addRollToLists() {
+    pPastRolls.push(currentPlayerRoll);
+    pPastRollsEl.innerHTML = pPastRolls;
+    cPastRolls.push(currentComputerRoll);
+    cPastRollsEl.innerHTML = cPastRolls;
+}
+
+function rollDice() {
+    return Math.floor(Math.random() * 20) + 1;
+}
+
+function displayGameMessage() {
+    gameMessageEl.innerText = `The computer rolled ${currentComputerRoll}.`;
+}
+
+function updatePlayerStats() {
+    pHealthBar -= currentComputerRoll;
+    pHealthNumber -= currentComputerRoll;
+    pHealthBarEl.style.width = pHealthBar + "%";
+    pHealthNumberEl.innerHTML = pHealthNumber;
+    let pHealthBarWidth = parseFloat(pHealthBarEl.style.width);
+    if (pHealthBarWidth <= 60 && pHealthBarWidth > 30) {
+        pHealthBarEl.style.backgroundColor = "yellow";
+    }
+    if (pHealthBarWidth <= 30) {
+        pHealthBarEl.style.backgroundColor = "red";
+    }
+    
+}
+
+function updateComputerStats() {
+    cHealthBar -= currentPlayerRoll ;
+    cHealthNumber -= currentPlayerRoll;
+    cHealthBarEl.style.width = cHealthBar + "%";
+    cHealthNumberEl.innerHTML = cHealthNumber;
+    let cHealthBarWidth = parseFloat(cHealthBarEl.style.width);
+    if (cHealthBarWidth <= 60 && cHealthBarWidth >30) {
+        cHealthBarEl.style.backgroundColor = "yellow";
+    }
+    if (cHealthBarWidth <= 30) {
+        cHealthBarEl.style.backgroundColor = "red";
+    }
+}
+
+function checkForWin() {
+
+    if (pHealthNumber <= 0 && cHealthNumber <= 0) {
+        pHealthNumberEl.innerHTML = 0;
+        pHealthBarEl.style.opacity = 0;
+        cHealthNumberEl.innerHTML = 0;
+        cHealthBarEl.style.opacity = 0;
+        gameMessageEl.innerText = `It's a draw! 
+        Press RESET to play again!`;
+        rollEl.setAttribute("disabled", "true");
+    } else if (pHealthNumber <= 0) {
+        pHealthNumberEl.innerHTML = 0;
+        pHealthBarEl.style.opacity = 0;
+        gameMessageEl.innerText = `You lost the battle...
+        Press RESET to try again!`;
+        rollEl.setAttribute("disabled", "true");
+    } else if (cHealthNumber <= 0) {
+        cHealthNumberEl.innerHTML = 0;
+        cHealthBarEl.style.opacity = 0;
+        gameMessageEl.innerText = `You defeated the ogre! 
+        Congratulations! 
+        Press RESET to play again!`
+        rollEl.setAttribute("disabled", "true");
+    }
+}
+
+function resetGame() {
+    rollEl.removeAttribute("disabled");
+    playerHealth = 100;
+    computerHealth = 100;
+    currentPlayer = "1";
+    currentPlayerRoll = 0;
+    currentComputerRoll = 0;
+    pHealthBar = 100;
+    pHealthNumber = 100;
+    cHealthBar = 100;
+    cHealthBarEl.style.opacity = 1;
+    cHealthBarEl.style.backgroundColor = "rgb(28, 104, 5)";
+    pHealthBarEl.style.backgroundColor = "rgb(28, 104, 5)"
+    pHealthBarEl.style.opacity = 1;
+    cHealthNumber = 100;
+    playerRollTotal = 0;
+    computerRollTotal = 0;
+    pPastRolls = [];
+    cPastRolls = [];
+    cHealthBarEl.style.width = "100%";
+    cHealthNumberEl.innerHTML = 100;
+    pHealthBarEl.style.width = "100%";
+    pHealthNumberEl.innerHTML = 100;
+    rollReadoutEl.innerHTML = "";
+    pPastRollsEl.innerHTML = "";
+    cPastRollsEl.innerHTML = "";
+    gameMessageEl.innerText = `You've been ambushed 
+    on your way to the castle!
+    Click ROLL to fight back!`;
+}
